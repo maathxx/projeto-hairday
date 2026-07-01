d9e5dd65fa2d92e30a25897bae94e17a8419b64f
Matheus <matheusfdeoliveira2006@gmail.com>
2026-07-01 12:53:45 -0300

    adding available times and the past time verification

---
+++ src/modules/form/hours-load.js
@@ -0,0 +1,22 @@
+import dayjs from "dayjs"
+
+import { openingHours } from "../../utils/opening-hours.js"
+
+export function hoursLoad({ date }) {
+    const opening = openingHours.map((hour) => {
+        // Recupera somente a hora.
+        const [scheduleHour] = hour.split(":")
+      
+
+
+        // Adiciona a hora na data e verifica se está no passado
+        const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
+       
+        return {
+            hour,
+            available: isHourPast,
+        } 
+
+    })
+
+}
--- src/utils/opening-hours.js
+++ src/utils/opening-hours.js
@@ -1,4 +1,4 @@
-const oppeningHours = [
+export const openingHours = [
     "9:00",
     "10:00",
     "11:00",
