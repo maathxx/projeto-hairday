3316eb327ee09302368e15f9f634a7f59fd9c857
Matheus <matheusfdeoliveira2006@gmail.com>
2026-08-21 12:20:50 -0300

    Blocking already scheduled times

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -5,20 +5,24 @@ import { hoursClick } from "../form/hours-click.js"
 
 const hours = document.getElementById("hours")
 
-export function hoursLoad({ date }) {
-    const opening = openingHours.map((hour) => {
+export function hoursLoad({ date, dailySchedules }) {
         // Limpa a lista de horários
         hours.innerHTML = ""
 
+        const unavailableHours = dailySchedules.map((schedule) => dayjs(schedule.when).format("HH:mm"))
+
+        const opening = openingHours.map((hour) => {
         // Recupera somente a hora.
         const [scheduleHour] = hour.split(":")
       
         // Adiciona a hora na data e verifica se está no passado
-        const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
+        const isHourPast = dayjs(date).add(scheduleHour, "hour").isBefore(dayjs())
+
+        const available = !unavailableHours.includes(hour) && !isHourPast
        
         return {
             hour,
-            available: isHourPast,
+            available,
         } 
 
     })
